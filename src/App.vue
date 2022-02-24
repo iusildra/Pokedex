2d07e0f5e520f7ce5a87f98735ad4bbd465fb148
Lucas Nouguier <lucas.nouguier@protonmail.com>
2022-02-24 02:02:27 +0100

    List navigation, pokemon's information
    Navigation by page number, max number of tiles and a tile to display information about a specific pokemon

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,15 @@ import Pokedex from "./components/Pokedex.vue";
 		:total="total"
 		:firstPage="offset - limit <= 0"
 		:lastPage="offset + limit >= total"
+		:search="search"
+		:detailledInfo="infos"
 		@next="getNextPokes()"
 		@previous="getNextPokes(this.offset, true)"
 		@first="getNextPokes(0)"
 		@last="getNextPokes(this.total - this.limit + 1)"
+		@limit="updateLimit"
+		@changePage="changePage"
+		@showPokemon="getDetailledInfo"
 	/>
 </template>
 
@@ -46,46 +51,49 @@ export default {
 			offset: 0,
 			limit: 25,
 			total: 0,
+			search: false,
+			infos: [],
 		};
 	},
 	mounted() {
 		this.getNextPokes();
 	},
 	methods: {
-		searchPoke(search) {
-			const addPoke = (name) => {
-				P.getPokemonByName(name).then((pokemon) => {
-					let svg = pokemon.sprites.other.dream_world.front_default;
-					let png = pokemon.sprites.other["official-artwork"].front_default;
-					let src = svg != null ? svg : png;
-					this.pokemons.push({
-						name: pokemon.name,
-						id: pokemon.id,
-						types: pokemon.types,
-						img: src,
-					});
+		addPoke(name) {
+			P.getPokemonByName(name).then((pokemon) => {
+				let svg = pokemon.sprites.other.dream_world.front_default;
+				let src =
+					svg != null
+						? svg
+						: pokemon.sprites.other["official-artwork"].front_default;
+				this.pokemons.push({
+					name: pokemon.name,
+					id: pokemon.id,
+					types: pokemon.types,
+					img: src,
 				});
-			};
-
+			});
+		},
+		searchPoke(search) {
 			this.offset = 0;
 			this.pokemons = [];
 
 			if (search.id > "") {
-				addPoke(search.id);
+				this.addPoke(search.id);
 			} else {
 				if (search.name > "") {
 					for (const pokemon of this.pokemonList) {
 						if (pokemon.name.includes(search.name)) {
-							addPoke(pokemon.name);
+							this.addPoke(pokemon.name);
 						}
 					}
 				} else {
-					for (let i = 0; i < this.limit; i++) {
-						addPoke(this.pokemonList[i].name);
-					}
-					this.offset += this.limit;
+					this.offset = 0;
+					this.getNextPokes();
 				}
 			}
+			if (search.id <= "" && search.name <= "") this.search = false;
+			else this.search = true;
 		},
 		async getNextPokes(offset = this.offset, prev = false) {
 			if (this.pokemonList.length == 0) {
@@ -103,7 +111,8 @@ export default {
 				P.getPokemonByName(this.pokemonList[i].name).then((pokemon) => {
 					let svg = pokemon.sprites.other.dream_world.front_default;
 					let png = pokemon.sprites.other["official-artwork"].front_default;
-					let src = svg != null ? svg : png;
+					let png_2 = pokemon.sprites.front_default;
+					let src = svg != null ? svg : png != null ? png : png_2;
 					this.pokemons.push({
 						name: pokemon.name,
 						id: pokemon.id,
@@ -113,6 +122,33 @@ export default {
 				});
 			}
 			this.offset = offset + this.limit;
+		},
+		async getDetailledInfo(id) {
+			await P.getPokemonByName(id).then((pokemon) => {
+				let svg = pokemon.sprites.other.dream_world.front_default;
+				let png = pokemon.sprites.other["official-artwork"].front_default;
+				let png_2 = pokemon.sprites.front_default;
+				let src = svg != null ? svg : png != null ? png : png_2;
+				this.infos = [
+					{
+						name: pokemon.name,
+						id: pokemon.id,
+						img: src,
+						types: pokemon.types,
+						abilities: pokemon.abilities,
+						weight: pokemon.weight,
+					},
+				];
+			});
+		},
+		updateLimit(limit) {
+			this.offset -= this.limit;
+			this.limit = limit;
+			this.getNextPokes();
+		},
+		changePage(n) {
+			this.offset = this.limit * (n - 1);
+			this.getNextPokes();
 		},
 	},
 };
--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,53 +1,88 @@
 <script setup>
 defineProps({
-	pokemons: Object,
+	pokemons: Array,
+	search: Boolean,
+	total: Number,
+	firstPage: Boolean,
+	lastPage: Boolean,
+	detailledInfo: Array,
 });
 </script>
 
 <template>
-	<div id="pokelist">
-		<div class="row">
-			<label for="nbMax">Nombre max à afficher</label>
-			<select id="nbMax" v-model="this.limit">
-				<option value="25" selected>25</option>
-				<option value="50">50</option>
-				<option value="75">75</option>
-				<option value="100">100</option>
-				<option value="125">125</option>
-			</select>
-		</div>
-		<div class="row">
-			<div class="page">
-				{{ this.firstPage }}
+	<div class="pokedex">
+		<div id="pokelist" class="col23">
+			<div class="row" v-if="!search">
+				<label for="nbMax">Nombre max à afficher</label>
+				<select id="nbMax" @change="updateLimit()" v-model="limit">
+					<option value="25" selected>25</option>
+					<option value="50">50</option>
+					<option value="75">75</option>
+					<option value="100">100</option>
+					<option value="125">125</option>
+				</select>
+			</div>
+			<div class="row" id="pages" v-if="!search">
+				<div
+					class="page"
+					v-for="page in parseInt(total / limit)"
+					:key="page"
+					@click="getPage(page)"
+				>
+					{{ page }}
+				</div>
+			</div>
+			<div class="tiles" v-for="pokemon of pokemons" ref="pokemon.id">
+				<img
+					:src="pokemon.img"
+					:alt="pokemon.name + ' picture'"
+					@click="showPokemon(pokemon.id)"
+				/>
+				<p name="id">#{{ pokemon.id }}</p>
+				<p name="name">{{ capitalize(pokemon.name) }}</p>
+				<p name="types">
+					<span v-for="slot in pokemon.types" :key="slot" class="type">{{
+						capitalize(slot.type.name)
+					}}</span>
+				</p>
+			</div>
+			<div class="row">
+				<div class="pageNav" @click="firstPoke">First</div>
+				<div class="pageNav" :disabled="firstPage" @click="previousPoke">
+					Prev
+				</div>
+				<div class="pageNav" :disabled="lastPage" @click="nextPoke">Next</div>
+				<div class="pageNav" @click="lastPoke">Last</div>
 			</div>
 		</div>
-		<div class="tiles" v-for="pokemon of pokemons" ref="pokemon.id">
-			<img :src="pokemon.img" alt="{{pokemon.name}}'s picture" />
-			<p name="id">#{{ pokemon.id }}</p>
-			<p>{{ capitalize(pokemon.name) }}</p>
-			<p>
-				<span v-for="slot in pokemon.types">{{
-					capitalize(slot.type.name)
-				}}</span>
-			</p>
-		</div>
-		<div class="row">
-			<div class="pageNav" @click="firstPoke">First</div>
-			<div class="pageNav" :disabled="firstPage" @click="previousPoke">Prev</div>
-			<div class="pageNav" :disabled="lastPage" @click="nextPoke">Next</div>
-			<div class="pageNav" @click="lastPoke">Last</div>
+		<div class="col13">
+			<div id="pokemonInfos" v-for="info in detailledInfo" :key="info.id">
+				<p>
+					<img :src="info.img" :alt="info.name + ' picture'" />
+				</p>
+				<p>#{{ info.id }}</p>
+				<p>{{ capitalize(info.name) }}</p>
+				<p>
+					<span v-for="slot in info.types" :key="slot" class="type">{{
+						capitalize(slot.type.name)
+					}}</span>
+				</p>
+				<p>
+					<span
+						v-for="ability in info.abilities"
+						:key="ability.ability.name"
+						class="ability"
+						>{{ capitalize(ability.ability.name) }}</span
+					>
+				</p>
+				<p>{{ info.weight }}kg</p>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-	props: {
-		pokemons: Array,
-		total: BigInt,
-		firstPage: Boolean,
-		lastPage: Boolean,
-	},
 	data() {
 		return {
 			limit: 25,
@@ -69,6 +104,15 @@ export default {
 		lastPoke() {
 			this.$emit("last");
 		},
+		updateLimit() {
+			this.$emit("limit", this.limit);
+		},
+		getPage(n) {
+			this.$emit("changePage", n);
+		},
+		showPokemon(id) {
+			this.$emit("showPokemon", id);
+		},
 	},
 };
 </script>
@@ -78,8 +122,7 @@ export default {
 	display: flex;
 	flex-wrap: wrap;
 	flex: 1 1 0;
-	margin: 0px 5%;
-	padding: 0px 5%;
+	padding: 0px 20px;
 	background-color: #eee;
 	justify-content: space-evenly;
 }
@@ -97,11 +140,18 @@ img {
 }
 
 span {
-	background-color: lightgreen;
 	border: 1px dotted black;
 	border-radius: 25px;
 	padding: 5px;
 	margin-right: 5px;
+	margin-bottom: 10px;
+}
+.type {
+	background-color: lightgreen;
+}
+
+.ability {
+	background-color: lightcoral;
 }
 
 select {
@@ -129,7 +179,52 @@ select {
 	margin-top: 10px;
 }
 
+.pokedex {
+	display: flex;
+}
 .page {
 	background-color: lightblue;
+	margin: 5px 5px;
+	padding: 3px;
+	border-radius: 5px;
+}
+
+#pages {
+	padding: 0px 50px;
+}
+
+template {
+	display: flex;
+	justify-content: space-around;
+}
+
+.col23,
+.col13 {
+	display: flex;
+	flex: 1 1 0;
+	justify-content: space-around;
+	align-content: space-around;
+}
+.col23 {
+	max-width: 60%;
+}
+.col13 {
+	max-width: 20%;
+}
+
+#pokemonInfos {
+	height: fit-content;
+	width: 250px;
+	position: sticky;
+	top: 30%;
+	right: 0;
+	background-color: lightblue;
+}
+
+#pokemonInfos p {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-evenly;
+	align-content: space-around;
 }
 </style>
